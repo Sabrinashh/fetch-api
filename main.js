b43d4286d28afc70a45a0535561b3c4434677add
Sabrinashh <110469877+Sabrinashh@users.noreply.github.com>
2022-10-31 22:18:27 +0400

    .

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 let btn=document.getElementById('btn')
-let table=document.querySelector('.table')
+let table=document.getElementById('tables')
 const baseUrl='https://jsonplaceholder.typicode.com'
 let getItems = ()=> {
     const fetchItems=fetch (`${baseUrl}/users`)
@@ -13,7 +13,7 @@ let getItems = ()=> {
             <td>${item.name}</td>
             <td>${item.username}</td>
             <td>${item.email}</td>
-           <td><button class="btn btn-danger btn-delete p-1 ">Delete</button> <button class="btn btn-success btn-edit p-1 ms-2 w-25">Edit</button></td>
+           <td><button id="delete" class="btn btn-danger btn-delete p-1 ">Delete</button> <button id="edit" class="btn btn-success btn-edit p-1 ms-2 w-25">Edit</button></td>
           </tr>
           </tbody>
       </table>`
@@ -21,3 +21,26 @@ let getItems = ()=> {
     return getItems;
 }
 btn.addEventListener('click',getItems)
+
+table.addEventListener('click',e=>{
+  if (e.target.id === "edit") {
+  let row=e.target.parentElement.parentElement;
+  let firstBox=row.children[1];
+  let first=firstBox.innerHTML;
+  let newValue=first;
+  console.log(first)
+firstBox.innerHTML=` <input type="text" value="${first}">`
+let input =firstBox.children[0];
+input.addEventListener('change',()=>{
+  newValue=input.value;
+  console.log(newValue)
+  firstBox.innerHTML =`${newValue}`
+})
+}
+})
+
+table.addEventListener('click',e=>{
+  if (e.target.id === "delete") {
+  e.target.parentElement.parentElement.remove();
+  }
+})
